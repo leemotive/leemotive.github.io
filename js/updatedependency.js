4e0c12eb8061619eb347efea14e942545396704d
leemotive <wstation@163.com>
2019-02-23 00:16:17 +0800

    更新同域下面库

---
+++ js/updatedependency.js
@@ -0,0 +1,10 @@
+// 更新package.json里面同域下面的库
+const package = require('./package.json');
+const exec = require('child_process').execSync;
+
+const devKeys = Object.keys(package.devDependencies).filter(key => key.startsWith('@name'));
+exec(`yarn add ${devKeys.join(' ')} --dev`);
+
+const keys = Object.keys(package.dependencies).filter(key => key.startsWith('@name'));
+exec(`yarn add ${keys.join(' ')}`);
+
