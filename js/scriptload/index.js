b12348eef27cd4bf577376570b6de135fd2eadb4
leemotive <wstation@163.com>
2020-02-22 15:29:14 +0800

    cache test

--- js/scriptload/index.js
+++ js/scriptload/index.js
@@ -11,6 +11,18 @@ app.get('/css.css', (req, res) => {
   console.log('return css', new Date());
 })
 
+app.get('/cachecss.css', (req, res) => {
+  // res.set('Expires', new Date('2021/02/23 00:00:00').toUTCString());
+  res.set('Cache-Control', 'public, max-age=0, must-revalidate');
+  res.sendFile(path.resolve(__dirname, 'css.css'));
+})
+
+app.get('/cachejs.js', (req, res) => {
+  // res.set('Expires', new Date('2021/02/23 00:00:00').toUTCString());
+  res.set('Cache-Control', 'public, max-age=60');
+  res.sendFile(path.resolve(__dirname, 'js.js'));
+})
+
 app.get('/', (req, res) => {
   res.set('Cache-Control', 'no-store');
   res.sendFile(path.resolve(__dirname, 'html.html'))
